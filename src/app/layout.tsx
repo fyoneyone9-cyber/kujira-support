41593afcc5f54b75d96679672b0fd3643c7a4cc5
fyoneyone9-cyber <f.yoneyone9@gmail.com>
2026-08-15 02:37:24 +0900

    favicon: クジラSVGファビコン追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'KUJIRA Support System',
   description: '導入支援サポートシステム',
+  icons: { icon: '/favicon.svg' },
 }
 
 export default function RootLayout({
